dc1404d444511d2fd09a1cc8faab6fc30131bd86
Keyaku <antonio.luis.sarmento@gmail.com>
2017-08-04 17:00:27 +0100

    Compilation fixes

--- SCRIPTS/RPG/Profile.gd
+++ SCRIPTS/RPG/Profile.gd
@@ -1,13 +1,9 @@
 extends Object
 
 # RPG Classes
-const RPG = "res://SCRIPTS/RPG/"
+const Level       = preload("res://SCRIPTS/RPG/Level.gd")
+const PlayerStats = preload("res://SCRIPTS/RPG/Stats/PlayerStats.gd")
 
-const Level       = preload(RPG + "Level.gd")
-const PlayerStats = preload(RPG + "Stats/PlayerStats.gd")
-
-# Constants
-const MEMBERS =
 
 # "Private" members
 var level = Level.new()
