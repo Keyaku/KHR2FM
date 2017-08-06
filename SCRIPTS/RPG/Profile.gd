92dee07cb788db0737944f14ea4651a909e2a429
Keyaku <antonio.luis.sarmento@gmail.com>
2017-08-06 01:22:07 +0100

    Profile: Added basic level_up()
    Simply increases base levels (exp or bonus) and emits a signal with all
    the abilities gained.

--- SCRIPTS/RPG/Profile.gd
+++ SCRIPTS/RPG/Profile.gd
@@ -1,5 +1,8 @@
 extends Object
 
+# Signals
+signal level_up(abilities)
+
 # RPG Classes
 const Level       = preload("res://SCRIPTS/RPG/Level.gd")
 const PlayerStats = preload("res://SCRIPTS/RPG/Stats/PlayerStats.gd")
@@ -7,14 +10,31 @@ const PlayerStats = preload("res://SCRIPTS/RPG/Stats/PlayerStats.gd")
 
 # "Private" members
 # Levels
-var exp_level   = Level.new([])
-var bonus_level = Level.new({})
+var exp_lv   = Level.new([])
+var bonus_lv = Level.new({})
 
+# Setting up minimal PlayerStats
 var stats = PlayerStats.new({
 	"lv" : 1, "bonus_lv" : 0,
 	"max_hp" : 20, "max_mp" : 10,
 	"str" : 1, "def" : 0,
 })
 
+###############
+### Methods ###
+###############
 func has(key):
-	return key in ["exp_level", "bonus_level", "stats"]
+	return key in ["exp_lv", "bonus_lv", "stats"]
+
+func level_up(stat, id=""):
+	var rewards = []
+	var lv = stats.get_base(stat)+1
+	stats.set_base(stat, lv)
+
+	if stat == "lv":
+		rewards = exp_lv.get(lv)
+		# TODO: set exp to 0, max_exp to the next value
+	elif stat == "bonus_lv" && not id.empty():
+		rewards = bonus_lv.get(id)
+
+	emit_signal("level_up", rewards)
